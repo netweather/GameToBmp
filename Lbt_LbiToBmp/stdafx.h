4d056c651a74a8879cee7cd5c5d53e6aeac5b307
netweather <qw2363101@163.com>
2015-08-05 22:29:16 +0800

    Lbt_LbiToBmp debug下有点小问题，其他的没事。 vs2013

---
+++ Lbt_LbiToBmp/stdafx.h
@@ -0,0 +1,29 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <fstream>
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+
+// TODO:  在此处引用程序需要的其他头文件
+/**********************************************************
+*********************LBT_LBI文件信息头*********************
+**********************************************************/
+typedef struct tagLBTIFILE {/*  */
+	WORD       bfType;							 //标识符
+	LONG       biWidth;							 //位图的宽度
+	LONG       biHeight;						 //位图的高度
+	WORD       biBitCount;						 //位图的位数
+} LBTIFILE;
